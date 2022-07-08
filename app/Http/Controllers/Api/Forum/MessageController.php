aaca387db6c4b1357646f13c8318f5efa698c013
jasonhoendervanger <hoendervanger@eur.nl>
2022-07-08 21:49:22 +0200

    initial commit

---
+++ app/Http/Controllers/Api/Forum/MessageController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api\Forum;
+
+use Illuminate\Http\JsonResponse;
+
+final class TopicController
+{
+    public function create(): JsonResponse
+    {
+
+    }
+
+    public function store(): JsonResponse
+    {
+
+    }
+
+    public function edit(): JsonResponse
+    {
+
+    }
+
+    public function update(): JsonResponse
+    {
+
+    }
+
+    public function destroy(): JsonResponse
+    {
+
+    }
+}
---
+++ app/Http/Controllers/Api/Forum/TopicController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api\Forum;
+
+use Illuminate\Http\JsonResponse;
+
+final class TopicController
+{
+    public function index(): JsonResponse
+    {
+
+    }
+
+    public function create(): JsonResponse
+    {
+
+    }
+
+    public function store(): JsonResponse
+    {
+
+    }
+
+    public function edit(): JsonResponse
+    {
+
+    }
+
+    public function update(): JsonResponse
+    {
+
+    }
+
+    public function destroy(): JsonResponse
+    {
+
+    }
+}
